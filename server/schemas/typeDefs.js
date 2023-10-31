c2d232e15275220d0f2123d4d7107e6c84b53851
Cameron <cameron@admins-mbp.mynetworksettings.com>
2023-10-31 16:39:38 -0400

    adding User, Auth and Query to typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -8,6 +8,23 @@ const typeDefs = `
         image: String
         link: String
     }
+    type User {
+        _id; ID
+        username: String
+        email: String
+        password: String
+        savedBooks: [Book]
+        bookCount: Int
+
+    }
+    type Auth {
+        token: ID!
+        profile: Profile
+    }
+    type Query {
+        user: User
+        users: [User]
+    }
 `
 
 module.exports = typeDefs;
